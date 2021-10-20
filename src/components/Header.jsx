b4f443d7eb22c649ca1662e3298d2d1385260ecf
Muneeb Ali <muneebali500@gmail.com>
2021-10-20 12:15:05 -0700

    changed website title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <h1 className="websiteTitle">CodePen</h1>
-      <p>Developed by Muneeb Ali as a Practice Project</p>
+      <p>Developed by Muneeb Ali to showcase his React skills</p>
       {/* <div className="headerBtns">
         <button onClick={() => saveToLocalStorage(data)}>Save</button>
         <button onClick={() => clearLocalStorage()}>Clear</button>
